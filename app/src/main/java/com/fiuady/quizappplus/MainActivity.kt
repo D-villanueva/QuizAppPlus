2071e6f7cdbce4a159a6b31282fa7be960fd4c96
Jellian <haanniiaarodriguez@gmail.com>
2021-01-05 16:24:52 -0600

    mejoras en pantalla de incio y se agrega nuevo boton de puntuaciones (Navigation view en proceso)
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

--- app/src/main/java/com/fiuady/quizappplus/MainActivity.kt
+++ app/src/main/java/com/fiuady/quizappplus/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.fiuady.quizappplus
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var TextInicio:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TextInicio=findViewById(R.id.titulo)
+        TextInicio.typeface= Typeface.createFromAsset(assets,"fonts/Balamoa.ttf")
     }
 }
