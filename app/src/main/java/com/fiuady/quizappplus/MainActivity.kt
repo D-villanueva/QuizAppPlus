83ec779613954448f324f85476a49a5028f6e7e2
Jellian <haanniiaarodriguez@gmail.com>
2021-01-05 17:45:10 -0600

    se añade el navigation view para colocar las opciones del perfil
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

--- app/src/main/java/com/fiuady/quizappplus/MainActivity.kt
+++ app/src/main/java/com/fiuady/quizappplus/MainActivity.kt
@@ -3,18 +3,37 @@ package com.fiuady.quizappplus
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var TextInicio:TextView
-
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        toggle= ActionBarDrawerToggle(this,drawer_layout,R.string.open,R.string.close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         TextInicio=findViewById(R.id.titulo)
         TextInicio.typeface= Typeface.createFromAsset(assets,"fonts/Balamoa.ttf")
     }
+
+    override fun onOptionsItemSelected(item:MenuItem):Boolean{
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
