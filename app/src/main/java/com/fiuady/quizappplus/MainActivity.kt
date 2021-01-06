e83aa659e629730eac9bab549de132dba4f669a1
Jellian <haanniiaarodriguez@gmail.com>
2021-01-05 18:21:59 -0600

    se agrega unicamente el portrait de Opciones
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

--- app/src/main/java/com/fiuady/quizappplus/MainActivity.kt
+++ app/src/main/java/com/fiuady/quizappplus/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.fiuady.quizappplus
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,10 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var TextInicio:TextView
     lateinit var toggle: ActionBarDrawerToggle
-
+    private lateinit var jugar_button: Button
+    private lateinit var opciones_button: Button
+    private lateinit var puntuaciones_button:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        jugar_button = findViewById(R.id.jugar_button)
+        opciones_button = findViewById(R.id.opciones_button)
+        puntuaciones_button = findViewById(R.id.puntaje_button)
 
 
         toggle= ActionBarDrawerToggle(this,drawer_layout,R.string.open,R.string.close)
@@ -26,6 +35,19 @@ class MainActivity : AppCompatActivity() {
 
         TextInicio=findViewById(R.id.titulo)
         TextInicio.typeface= Typeface.createFromAsset(assets,"fonts/Balamoa.ttf")
+
+        jugar_button.setOnClickListener { _ ->
+            val game = Intent(this, game::class.java)
+            startActivity(game)
+        }
+
+        opciones_button.setOnClickListener { _ ->
+            val option = Intent(this@MainActivity, Opciones::class.java)
+
+            startActivity(option)
+
+        }
+
     }
 
     override fun onOptionsItemSelected(item:MenuItem):Boolean{
---
+++ app/src/main/java/com/fiuady/quizappplus/Opciones.kt
@@ -0,0 +1,11 @@
+package com.fiuady.quizappplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Opciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opciones)
+    }
+}
